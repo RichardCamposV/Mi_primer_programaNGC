8aac7676a2a09dd689b8c534d3964b903b7bab7c
RicardoCamposV <ricard_halo@live.com.mx>
2019-07-04 00:55:43 -0500

    Añadiendo ejercicio curso funciones.

---
+++ excercise_function_maxthree.py
@@ -0,0 +1,26 @@
+"""
+Ejercicio 1
+Escribe una función que encuentre el numero más grande entre 3 numeros
+"""
+
+
+def max_number(numbers):
+    list_number = []
+    confirmation = False
+    while not confirmation:
+        user_number = input(numbers)
+        if user_number.isdigit():
+            list_number.append(int(user_number))
+            sure = input("¿Quieres seguir agregando numeros? [Si / No]: ").upper()
+            if sure != "SI":
+                confirmation = True
+    list_number = max(list_number)
+
+    return list_number
+
+
+print(max_number("Dime tu numero: "))
+
+
+
+
